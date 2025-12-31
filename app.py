584446095ac207887967842e6f0919346e440f44
vrushabhsakhiya <vrushabh@satvarth@gmail.com>
2025-12-31 12:02:53 +0530

    instance change

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def create_app():
             db.session.commit()
 
     return app
-
+app = create_app()
 if __name__ == '__main__':
-    app = create_app()
+    
     app.run(debug=True, port=5000)
